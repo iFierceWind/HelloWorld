e114fbf71955d441fc63e0fe4e2c78bfd1f90168
ligen-bwf <ligen@51testing.com>
2018-03-23 17:17:28 +0800

    org

--- untitled/src/com/hello/world/HelloWorld.java
+++ untitled/src/com/hello/world/HelloWorld.java
@@ -5,6 +5,6 @@ package com.hello.world;
  */
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello World \r\n 你好 天朝！");
+        System.out.println("Hello World \r\n 你好 天朝！！");
     }
 }
